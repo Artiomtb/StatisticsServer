f9e7790d6504d95dec9bd9840519b7403bd40fb0
Artiomtb <artiomtb@gmail.com>
2014-12-29 17:42:55 +0200

    Revert "Changed type of QueryParameters collection from ArrayList<QueryParameter> to Collection<QueryParameter>"
    This reverts commit 4b7c0219977345fefbeacf24ff966aeb5172b124.

--- src/main/java/model/ConnectionsHandler.java
+++ src/main/java/model/ConnectionsHandler.java
@@ -1,13 +1,13 @@
 package model;
 
 import java.sql.*;
-import java.util.Collection;
+import java.util.ArrayList;
 
 public class ConnectionsHandler {
     private PreparedStatement preparedStatement;
     private ResultSet resultSet;
 
-    public ConnectionsHandler(Connection connection, String query, Collection<QueryParameter> params) throws SQLException {
+    public ConnectionsHandler(Connection connection, String query, ArrayList<QueryParameter> params) throws SQLException {
         this.preparedStatement = connection.prepareStatement(query);
         setQueryParams(params);
         this.resultSet = this.preparedStatement.executeQuery();
@@ -24,7 +24,7 @@ public class ConnectionsHandler {
         this.resultSet = this.preparedStatement.executeQuery();
     }
 
-    private void setQueryParams(Collection<QueryParameter> params) throws SQLException {
+    private void setQueryParams(ArrayList<QueryParameter> params) throws SQLException {
         int index = 1;
         for (QueryParameter parameter : params) {
             setQueryParam(parameter, index++);
