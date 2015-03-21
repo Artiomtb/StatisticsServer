efc7753c4788bc770c3f89bd8b716443fed1c15b
Oleksandr <rocketmannot@gmail.com>
2015-03-21 15:24:28 +0200

    fix case search

--- src/main/webapp/assets/js/modules/main/directives/boldText.ts
+++ src/main/webapp/assets/js/modules/main/directives/boldText.ts
@@ -2,7 +2,7 @@ function boldText ($interpolate) {
     return {
         restrict: 'E',
         link: function (scope, element, attrs) {
-            var boldTextReg = new RegExp(attrs.boldText, 'g');
+            var boldTextReg = new RegExp(attrs.boldText, 'gi');
             var resHtml  = attrs.text.replace(boldTextReg, "<b class='boldClass'>" + attrs.boldText + "</b>");
             var fun = $interpolate(resHtml);
             element.html(fun({boldClass: attrs.boldClass}));
