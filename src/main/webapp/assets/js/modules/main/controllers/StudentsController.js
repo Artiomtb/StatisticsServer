af740645c09471b600f996181df6d0eb79cb0d6c
Oleksandr <rocketmannot@gmail.com>
2015-03-30 21:13:46 +0300

    add search bar to students page

--- src/main/webapp/assets/js/modules/main/controllers/StudentsController.js
+++ src/main/webapp/assets/js/modules/main/controllers/StudentsController.js
@@ -1,13 +1,17 @@
 /// <reference path="../../../shared/interfaces.ts" />
 define(["require", "exports"], function (require, exports) {
     var StudentsController = (function () {
-        function StudentsController(PATH_CONSTANTS, $scope, $http, $routeParams) {
+        function StudentsController(PATH_CONSTANTS, $scope, $http, $routeParams, Search, SEARCH_OPTIONS) {
             var _this = this;
             this.PATH_CONSTANTS = PATH_CONSTANTS;
             this.$scope = $scope;
             this.$http = $http;
             $scope.page_path = PATH_CONSTANTS.STUDENTS_PATH;
             $scope.student_path = PATH_CONSTANTS.STUDENT;
+            $scope.options = {
+                default: SEARCH_OPTIONS.STUDENT,
+                params: [{ value: SEARCH_OPTIONS.STUDENT, name: "Студенти", isActive: true, searchHandler: Search.searchStudentsHandler, autocompleteHandler: Search.autoCompleteStudentsHandler, resultNavPath: Search.getStudentsPath() }, { value: SEARCH_OPTIONS.PUBS, name: "Дисципліни", isActive: false, searchHandler: Search.searchPubsHandler, autocompleteHandler: Search.autoCompletePubsHandler, resultNavPath: Search.getPubsResultsPath() }]
+            };
             $http.get(PATH_CONSTANTS.STUDENTS_PATH, { params: { page: $routeParams.page } }).success(function (students) {
                 _this.$scope.students = students.students;
                 _this.$scope.pages = students.pages;
@@ -16,7 +20,7 @@ define(["require", "exports"], function (require, exports) {
                 console.log("some error occured");
             });
         }
-        StudentsController.$inject = ['PATH_CONSTANTS', '$scope', '$http', "$routeParams"];
+        StudentsController.$inject = ['PATH_CONSTANTS', '$scope', '$http', "$routeParams", "Search", "SEARCH_OPTIONS"];
         return StudentsController;
     })();
     return StudentsController;
--- src/main/webapp/assets/js/modules/main/controllers/StudentsController.ts
+++ src/main/webapp/assets/js/modules/main/controllers/StudentsController.ts
@@ -1,11 +1,24 @@
 /// <reference path="../../../shared/interfaces.ts" />
 
 class StudentsController {
-    static $inject = ['PATH_CONSTANTS','$scope','$http',"$routeParams"];
+    static $inject = ['PATH_CONSTANTS','$scope','$http',"$routeParams", "Search", "SEARCH_OPTIONS"];
 
-    constructor (private PATH_CONSTANTS, private $scope, private $http, $routeParams){
+    constructor (private PATH_CONSTANTS, private $scope, private $http, $routeParams, Search: ISearchService, SEARCH_OPTIONS){
         $scope.page_path = PATH_CONSTANTS.STUDENTS_PATH;
         $scope.student_path = PATH_CONSTANTS.STUDENT;
+        $scope.options = {
+            default: SEARCH_OPTIONS.STUDENT,
+            params: [{value: SEARCH_OPTIONS.STUDENT, name: "Студенти",
+                isActive: true,
+                searchHandler: Search.searchStudentsHandler,
+                autocompleteHandler: Search.autoCompleteStudentsHandler,
+                resultNavPath: Search.getStudentsPath()},
+                {value: SEARCH_OPTIONS.PUBS, name: "Дисципліни",
+                    isActive: false,
+                    searchHandler: Search.searchPubsHandler,
+                    autocompleteHandler: Search.autoCompletePubsHandler,
+                    resultNavPath: Search.getPubsResultsPath() }]};
+
         $http.get(PATH_CONSTANTS.STUDENTS_PATH,{params: {page: $routeParams.page}})
             .success((students: IStudents) => {
                 this.$scope.students=students.students;
